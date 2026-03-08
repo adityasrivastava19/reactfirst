ada2ea0b6e68ac8c3a82a64f01249cd93d822d6c
Aditya Srivastava <aditya123@gmail.com>
2026-03-08 23:22:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(function()
+{
+  setInterval(function(){
+    setCount(function(count){
+      return count+1;
+    })
+  },1000)
+},[]);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  )
+}
+
+export default App
